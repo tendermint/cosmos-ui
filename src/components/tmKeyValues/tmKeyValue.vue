19e26f0c0ce384392929f07275a7174ce2ef443b
Billy Rennekamp <billy.rennekamp@gmail.com>
2018-06-13 14:46:02 +0200

    tmKeyValues

---
+++ src/components/tmKeyValues/tmKeyValue.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="tm-kv">
+    <div class="tm-kv-container">
+      <div class="tm-kv-value"><slot name="value"></slot></div>
+      <div class="tm-kv-key"><slot name="key"></slot></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "tm-kv"
+}
+</script>
+
+<style lang="stylus">
+
+.tm-kv
+  display flex
+  flex-flow column no-wrap
+  align-items center
+  justify-content center
+  padding 1rem
+
+.tm-kv-value
+.tm-kv-key
+  display flex
+  justify-content center
+
+.tm-kv-value
+  font-size 1.25rem
+  line-height 1
+  margin-bottom 0.75rem
+  font-weight 400
+
+.tm-kv-key
+  color #999
+  font-size 0.75rem
+  font-weight 400
+  line-height 1
+
+@media screen and (min-width: 414px)
+  .tm-kv
+    padding 0.75rem 1rem
+
+@media screen and (min-width: 768px)
+  .tm-kv
+    padding 1.5rem
+    
+  .tm-kv-value
+    font-size 1.5rem
+    margin-bottom 1rem
+
+  .tm-kv-key
+    font-size 1rem
+    font-weight 300
+</style>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import TmHelloWorld from "./components/tmHelloWorld"
 import TmField from "./components/tmField"
+import { TmKeyValues } from "./components/tmKeyValues"
+import { TmKeyValue } from "./components/tmKeyValues"
 
 const TendermintComponents = {
   TmHelloWorld,
-  TmField
+  TmField,
+  TmKeyValue,
+  TmKeyValues
 }
 
 TendermintComponents.install = Vue =>
@@ -18,3 +22,7 @@ export {
   default as TmHelloWorld
 } from "./components/tmHelloWorld/tmHelloWorld.vue"
 export { default as TmField } from "./components/tmField/tmField.vue"
+export { default as TmKeyValue } from "./components/TmKeyValues/TmKeyValue.vue"
+export {
+  default as TmKeyValues
+} from "./components/TmKeyValues/TmKeyValues.vue"
