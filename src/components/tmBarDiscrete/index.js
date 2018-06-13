c6c512dbd6c7bfe0ab74bf94d080875769bee2a0
Billy Rennekamp <billy.rennekamp@gmail.com>
2018-06-13 16:44:41 +0200

    TmBarDiscrete

---
+++ src/components/tmBarDiscrete/index.js
@@ -0,0 +1,5 @@
+import tmBarDiscrete from "./tmBarDiscrete.vue"
+
+export default function install(Vue) {
+  Vue.component("tm-bar-discrete", tmBarDiscrete)
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import TmHelloWorld from "./components/tmHelloWorld"
 import TmField from "./components/tmField"
+import TmBarDiscrete from "./components/tmBarDiscrete"
 
 const TendermintComponents = {
   TmHelloWorld,
-  TmField
+  TmField,
+  TmBarDiscrete
 }
 
 TendermintComponents.install = Vue =>
@@ -18,3 +20,6 @@ export {
   default as TmHelloWorld
 } from "./components/tmHelloWorld/tmHelloWorld.vue"
 export { default as TmField } from "./components/tmField/tmField.vue"
+export {
+  default as TmBarDiscrete
+} from "./components/tmBarDiscrete/tmBarDiscrete.vue"
