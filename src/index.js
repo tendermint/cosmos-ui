b561ca716f0aa6bf270744c2c8b8d7f17725425c
Billy Rennekamp <billy.rennekamp@gmail.com>
2018-06-13 17:45:51 +0200

    list item

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import TmHelloWorld from "./components/tmHelloWorld"
 import TmField from "./components/tmField"
+import TmListItem from "./components/TmListItem"
 
 const TendermintComponents = {
   TmHelloWorld,
-  TmField
+  TmField,
+  TmListItem
 }
 
 TendermintComponents.install = Vue =>
@@ -18,3 +20,4 @@ export {
   default as TmHelloWorld
 } from "./components/tmHelloWorld/tmHelloWorld.vue"
 export { default as TmField } from "./components/tmField/tmField.vue"
+export { default as TmListItem } from "./components/TmListItem/TmListItem.vue"
