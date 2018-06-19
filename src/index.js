18e4ebeee3476cc9c935c3bc43de71790ac19310
Peng Zhong <172531+nylira@users.noreply.github.com>
2018-06-19 19:54:51 +0800

    remove root index.js

---
+++ src/index.js
@@ -0,0 +1,23 @@
+module.exports = {
+  TmField: require("./components/TmField/TmField.vue").default,
+  TmNotifications: require("./components/TmNotifications/TmNotifications.vue")
+    .default,
+  TmCountdown: require("./components/TmCountdown/TmCountdown.vue").default,
+  TmKeyValues: require("./components/TmKeyValues/TmKeyValues.vue").default,
+  TmKeyValue: require("./components/TmKeyValues/TmKeyValue.vue").default,
+  TmModules: require("./components/TmModules/TmModules.vue").default,
+  TmModule: require("./components/TmModules/TmModule.vue").default,
+  TmPart: require("./components/TmPart/TmPart.vue").default,
+  TmPage: require("./components/TmPage/TmPage.vue").default,
+  TmPageHeader: require("./components/TmPageHeader/TmPageHeader.vue").default,
+  TmPageFooter: require("./components/TmPageFooter/TmPageFooter.vue").default,
+  TmFormStruct: require("./components/TmFormStruct/TmFormStruct.vue").default,
+  TmFormGroup: require("./components/TmFormGroup/TmFormGroup.vue").default,
+  TmFieldGroup: require("./components/TmFieldGroup/TmFieldGroup.vue").default,
+  TmDataMsg: require("./components/TmDataMsg/TmDataMsg.vue").default,
+  TmFormMsg: require("./components/TmFormMsg/TmFormMsg.vue").default,
+  TmBarDiscrete: require("./components/TmBarDiscrete/TmBarDiscrete.vue")
+    .default,
+  TmListItem: require("./components/TmListItem/TmListItem.vue").default,
+  TmBtn: require("./components/TmBtn/TmBtn.vue").default
+}
