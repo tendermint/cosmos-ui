ab6c9cb5ae6aafbb1807090107e721c366864a61
Billy Rennekamp <billy.rennekamp@gmail.com>
2018-07-11 13:40:06 +0200

    fix unit test around TmTextMarkdown

--- tests/unit/TmTextMarkdown.spec.js
+++ tests/unit/TmTextMarkdown.spec.js
@@ -1,14 +1,31 @@
 import { shallowMount } from "@vue/test-utils"
-import htmlBeautify from "html-beautify"
 import { TmTextMarkdown } from "@/index"
+jest.mock("axios", () => ({
+  get: url =>
+    url &&
+    Promise.resolve({
+      data: {
+        content: global.btoa(`## hello!
 
+\`code\`
+
+[link](https://google.com)`)
+      }
+    })
+}))
 describe("TmTextMarkdown.vue", () => {
   let wrapper
+
   beforeEach(async () => {
     wrapper = shallowMount(TmTextMarkdown)
   })
 
-  it("has the expected html structure", () => {
-    expect(htmlBeautify(wrapper.html())).toMatchSnapshot()
+  it("has the expected html structure", async () => {
+    expect(wrapper.vm.$el).toMatchSnapshot()
+    wrapper.setProps({
+      url: "https://google.com"
+    })
+    await wrapper.vm.$nextTick()
+    expect(wrapper.vm.$el).toMatchSnapshot()
   })
 })
