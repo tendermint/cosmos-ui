6b9832947105c9ccb58cc3c0768e6e9c21f93897
Cyrus Goh <hello@lovincyrus.com>
2019-09-21 23:56:06 -0700

    Refactor file structure

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,10 @@
 import { configure } from '@storybook/vue'
 
+// automatically import all files ending in *.stories.js
+const req = require.context("../src", true, /\.stories\.(js?)$/);
+
 function loadStories() {
-  require('../src/stories')
+  req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module)
