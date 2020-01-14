91a2961a86ae592998a8dc71685b3f691618137a
Cyrus Goh <hello@lovincyrus.com>
2020-01-13 16:11:05 -0800

    refactor

--- src/PageHeader/PageHeader.vue
+++ src/PageHeader/PageHeader.vue
@@ -2,25 +2,22 @@
   <div class="page-header">
     <div class="page-header__container">
       <div class="page-header__text">
-        <div class="page-header__suptitle" v-if="$slots['suptitle'] || suptitle">
+        <div class="page-header__suptitle">
           <template v-if="suptitle">{{ suptitle }}</template>
           <slot v-else name="suptitle"></slot>
         </div>
-        <div class="page-header__project-title" v-if="$slots['project-title'] || projectTitle">
+        <div class="page-header__project-title">
           <template v-if="projectTitle">{{ projectTitle }}</template>
           <slot v-else name="project-title"></slot>
         </div>
-        <div class="page-header__title h1" v-if="$slots['title'] || title">
+        <div class="page-header__title h1">
           <template v-if="title">{{ title }}</template>
           <slot v-else name="title"></slot>
         </div>
-        <div class="page-header__subtitle" v-if="$slots['subtitle'] || subtitle">
+        <div class="page-header__subtitle">
           <template v-if="subtitle">{{ subtitle }}</template>
           <slot v-else name="subtitle"></slot>
         </div>
-      </div>
-      <div class="page-header__menu">
-        <slot></slot>
       </div>
     </div>
   </div>
@@ -41,7 +38,7 @@ export default {
   padding: 2rem;
 }
 
-.page-header__text + .page-header__menu {
+.page-header__text {
   margin-top: 1.5rem;
 }
 
@@ -49,8 +46,7 @@ export default {
   margin-bottom: 1rem;
 }
 
-.page-header__title,
-.h1 {
+.page-header__title, .h1 {
   font-size: 2.5rem;
   line-height: 1.16;
   font-weight: 300;
@@ -73,63 +69,21 @@ export default {
   color: hsla(0, 100%, 0%, 0.8);
 }
 
-.page-header__menu {
-  margin: -0.25rem;
-}
-
-.page-header__menu.tm-btn {
-  margin: 0.25rem;
-}
-
-.page-header__menu:empty {
-  display: none;
-}
-
-@media screen and (min-width: 768px) {
+@media screen and (max-width: 1680px) {
   .page-header__container {
-    padding-top: 5.75rem;
-    padding-left: 5.75rem;
-    padding-right: 5.75rem;
-  }
-
-  .page-header__project-title {
-    margin-top: -2.5rem;
+    padding: 5.75rem 8rem 2rem;
   }
 }
 
-@media screen and (min-width: 1024px) {
+@media screen and (max-width: 1024px) {
   .page-header__container {
-    display: flex;
-  }
-
-  .page-header__text {
-    flex: 2;
-  }
-
-  .page-header__menu {
-    flex: 1;
-    margin-top: 0;
-    display: flex;
-    align-items: center;
-    justify-content: flex-end;
-  }
-
-  .page-header__text + .page-header__menu {
-    margin-top: 0;
+    padding: 5.75rem 5.75rem 2rem;
   }
 }
 
-@media screen and (min-width: 1280px) {
+@media screen and (max-width: 768px) {
   .page-header__container {
-    padding-left: 8rem;
-    padding-right: 8rem;
-  }
-}
-
-@media screen and (min-width: 1920px) {
-  .page-header__container {
-    padding-left: 12rem;
-    padding-right: 12rem;
+    padding: 2rem;
   }
 }
 </style>
