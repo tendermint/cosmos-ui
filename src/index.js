8e185279c5122fdb4c7e777af0f1695867a847de
Cyrus Goh <hello@lovincyrus.com>
2020-01-22 13:06:36 -0800

    Export components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-import GlobalStyles from "./GlobalStyles/GlobalStyles";
-import Modal from "./Modal/Modal";
-import Tooltip from "./Tooltip/Tooltip";
-import PageHeader from "./PageHeader/PageHeader";
-import CookieBanner from "./CookieBanner/CookieBanner";
-import Sidebar from "./Sidebar/Sidebar"
-import Footer from "./Footer/Footer"
-
-export { GlobalStyles, Modal, Tooltip, CookieBanner, Sidebar, Footer, PageHeader };
+export * from "./GlobalStyles";
+export * from "./Modal";
+export * from "./Tooltip";
+export * from "./PageHeader";
+export * from "./CookieBanner";
+export * from "./Sidebar";
+export * from "./Footer";
