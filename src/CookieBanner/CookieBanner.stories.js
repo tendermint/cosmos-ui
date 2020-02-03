3efa7467182ef9d6e117614e3d60cd4020bf0663
Denis Fadeev <denis@fadeev.org>
2020-02-03 13:17:54 +0500

    Add reset banner button to CookieBanner component

--- src/CookieBanner/CookieBanner.stories.js
+++ src/CookieBanner/CookieBanner.stories.js
@@ -1,4 +1,5 @@
 import CookieBanner from "./CookieBanner.vue";
+import { remove } from 'tiny-cookie';
 
 export default {
   title: "CookieBanner"
@@ -6,14 +7,21 @@ export default {
 
 export const normal = () => ({
   components: { CookieBanner },
-  data: function() {
+  data: function () {
     return {
       banner: true
     };
   },
   template: `
     <div>
+      <button @click="reset">Reset the banner</button>
       <cookie-banner />
     </div>
-  `
+  `,
+  methods: {
+    reset() {
+      remove('cookie-consent-accepted')
+      location.reload()
+    }
+  }
 });
