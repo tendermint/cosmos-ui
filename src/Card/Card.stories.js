5cc675fe45e06f3695c552d184225c703f89d133
Denis Fadeev <denis@fadeev.org>
2020-02-07 17:18:38 +0500

    Card with knobs WIP

--- src/Card/Card.stories.js
+++ src/Card/Card.stories.js
@@ -1,5 +1,5 @@
 import Card from "./Card.vue";
-import { withKnobs } from '@storybook/addon-knobs';
+import { withKnobs, text } from '@storybook/addon-knobs';
 
 export default {
   title: "Card",
@@ -9,9 +9,25 @@ export default {
 
 export const normal = () => ({
   components: { Card },
+  props: {
+    overline: {
+      default: text("Overline", "Overline")
+    },
+    title: {
+      default: text("Title", "Title")
+    },
+    size: {
+      default: text("Size", "medium")
+    }
+  },
   template: `
-    <div>
-      <card/>
+    <div style="display: grid; grid-auto-flow: column; gap: 1rem;">
+      <card v-bind="{overline, title, size}">
+        <div>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</div>
+      </card>
+      <card v-bind="{overline, title, size}">
+        <div>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</div>
+      </card>
     </div>
   `
 });
--- src/Card/Card.vue
+++ src/Card/Card.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
-    <div class="card">
-      <div class="overline">
+    <div class="card" :style="cardStyle">
+      <div v-if="overline" class="overline">
         {{overline}}
       </div>
-      <div class="title">
+      <div v-if="title" class="title">
         {{title}}
+      </div>
+      <div class="body">
+        <slot/>
       </div>
     </div>
   </div>
@@ -13,9 +16,41 @@
 
 <style scoped>
 .card {
-  display: inline-block;
-  box-shadow: var(--ds-elevation-1);
-  padding: 1rem;
+  display: block;
+  padding: calc(var(--card-size) - 4px) var(--card-size);
+  font-family: var(--ds-font-family, sans-serif);
+  border-radius: 0.5rem;
+  position: relative;
+  background: white;
+}
+.card:before {
+  border-radius: 0.5rem;
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: -1;
+  box-shadow: var(--ds-elevation-6);
+}
+.title {
+  font-size: var(--card-title-font-size, 1rem);
+  font-weight: 500;
+  line-height: var(--card-title-line-height, 1.5em);
+  color: #161931;
+}
+.overline {
+  font-size: .75rem;
+  text-transform: uppercase;
+  color: #67697a;
+  letter-spacing: 0.2em;
+}
+.body {
+  color: rgba(22, 25, 49, 0.65);
+  font-size: var(--card-body-font-size, 14px);
+  line-height: var(--card-body-line-height, 1.5em);
+  letter-spacing: 0.01em;
 }
 </style>
 
@@ -30,9 +65,26 @@ export default {
       type: String,
       default: "Title"
     },
-    padding: {
+    size: {
       type: String,
-      default: "24px"
+      default: "16px"
+    },
+  },
+  computed: {
+    cardStyle() {
+      const keywords = {
+        "small": "16px",
+        "medium": "24px",
+        "large": "32px"
+      }
+      const size = parseInt(keywords[this.size] || this.size)
+      return {
+        "--card-size": size + 'px',
+        "--card-title-font-size": size <= 18 ? "16px" : "20px",
+        "--card-title-line-height": size <= 18 ? "24px" : "28px",
+        "--card-body-font-size": size <= 18 ? "13px" : "14px",
+        "--card-body-line-height": size <= 18 ? "18px" : "20px"
+      }
     }
   }
 }
