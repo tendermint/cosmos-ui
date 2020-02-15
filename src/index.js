76a3ba132bdb09a5b5ebf6dee2c6e2ed26675bdd
Denis Fadeev <denis@fadeev.org>
2020-02-15 21:50:12 +0500

    CodeBlock WIP

--- src/index.js
+++ src/index.js
@@ -4,3 +4,4 @@ export { default as PageHeader } from "./PageHeader/PageHeader";
 export { default as CookieBanner } from "./CookieBanner/CookieBanner";
 export { default as Sidebar } from "./Sidebar/Sidebar";
 export { default as Footer } from "./Footer/Footer";
+export { default as CodeBlock } from "./CodeBlock/CodeBlock";
