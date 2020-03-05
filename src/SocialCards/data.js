86b55ccb03272e5fe068b96c746f5bc9be3c451e
Cyrus Goh <hello@lovincyrus.com>
2020-03-05 14:43:53 -0800

    Replace icons with github icon

--- src/SocialCards/data.js
+++ src/SocialCards/data.js
@@ -1,19 +1,19 @@
 export default {
   social_cards: [
     {
-      "icon": "https://cosmos.network/img/matrix.464d34e7.svg",
+      "icon": "https://cosmos.network/img/github.f5a069d2.svg",
       "h1": "Cool Title",
       "h2": "This is a very cool description",
       "href": "https://riot.im/app/#/room/#cosmos-sdk:matrix.org"
     },
     {
-      "icon": "https://cosmos.network/img/discourse.6efb74b8.svg",
+      "icon": "https://cosmos.network/img/github.f5a069d2.svg",
       "h1": "Cool Title",
       "h2": "This is a very cool description",
       "href": "https://forum.cosmos.network"
     },
     {
-      "icon": "https://cosmos.network/img/reddit.44b363b6.svg",
+      "icon": "https://cosmos.network/img/github.f5a069d2.svg",
       "h1": "Cool Title",
       "h2": "This is a very cool description",
       "href": "https://www.reddit.com/r/cosmosnetwork"
@@ -25,13 +25,13 @@ export default {
       "href": "https://github.com/cosmos/cosmos-sdk/"
     },
     {
-      "icon": "https://cosmos.network/img/twitter.c2ebe131.svg",
+      "icon": "https://cosmos.network/img/github.f5a069d2.svg",
       "h1": "Cool Title",
       "h2": "This is a very cool description",
       "href": "https://twitter.com/cosmos"
     },
     {
-      "icon": "https://cosmos.network/img/youtube.7c218bf3.svg",
+      "icon": "https://cosmos.network/img/github.f5a069d2.svg",
       "h1": "Cool Title",
       "h2": "This is a very cool description",
       "href": "https://www.youtube.com/channel/UC8HFOUdnMnpoWmQMgeKoB3A"
