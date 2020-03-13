e487ee0c8b2e8672f5017280be0c90ce2d5457ea
Denis Fadeev <denis@fadeev.org>
2020-03-13 16:02:01 +0500

    ModalSearch WIP

---
+++ src/SectionSearch/ModalSearchShowcase.vue
@@ -0,0 +1,17 @@
+<template>
+  <modal-sheet :visible="true">
+
+  </modal-sheet>
+</template>
+
+<script>
+import Modal from "../Modal/Modal.vue"
+import SectionSearch from "./SectionSearch.vue"
+
+export default {
+  components: {
+    SectionSearch,
+    "modal-sheet": Modal
+  }
+}
+</script>
