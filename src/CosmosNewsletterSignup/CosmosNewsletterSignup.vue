cf800dc54677a86df7cf48bfe1a1e144d913ec34
Denis Fadeev <denis@fadeev.org>
2020-03-30 11:16:18 +0500

    Transitions WIP

--- src/CosmosNewsletterSignup/CosmosNewsletterSignup.vue
+++ src/CosmosNewsletterSignup/CosmosNewsletterSignup.vue
@@ -8,31 +8,47 @@
           </div>
         </div>
         <div class="text">
-          <div class="page">
-            <div class="h1">Stay tuned for more</div>
-            <div class="p1">Get the latest from the Cosmos ecosystem and engineering updates, straight to your inbox.</div>
-            <div class="email__form">
-              <div class="email__form__input">
-                <input class="email__form__input__input" type="text" placeholder="Your email">
+          <transition name="forwards">
+            <div class="page" v-if="step === 0" key="1">
+              <div>
+                <div class="h1">Stay tuned for more</div>
+                <div class="p1">Get the latest from the Cosmos ecosystem and engineering updates, straight to your inbox.</div>
+                <div class="email__form">
+                  <div class="email__form__input">
+                    <input class="email__form__input__input" type="text" placeholder="Your email">
+                  </div>
+                  <ds-button @click.native="actionSubmit">
+                    Sign up
+                    <template v-slot:right>
+                      <icon-arrow-right/>
+                    </template>
+                  </ds-button>
+                </div>
+                <div class="p2">
+                  You can unsubscribe at any time. <a href="https://cosmos.network/privacy" target="_blank">Privacy Policy</a>
+                </div>
               </div>
-              <div class="email__form__button">
-                <ds-button size="m" :disabled="true">
-                  Sign up
+            </div>
+            <div class="page" v-else-if="step === 1" key="2">
+              <div>
+                <ds-button size="s" backgroundColor="rgba(0,0,0,0)" type="text" @click.native="actionBack">
+                  <template v-slot:left>
+                    <icon-chevron-left/>
+                  </template>
+                  Back
                 </ds-button>
+                <div class="h2">What are you interested in?</div>
+                <div class="buttons">
+                  <div class="buttons__button"></div>
+                </div>
               </div>
             </div>
-          </div>
-          <!-- <div class="page">
-            <div class="h2">What are you interested in?</div>
-            <div class="buttons">
-              <div class="buttons__button"></div>
+            <div class="page" v-else-if="step === 2" key="3">
+              <div class="h1">Almost there…</div>
+              <div class="p1">You should get a confirmation email for each of your selected interests. Open it up and click ‘Confirm Subscription’ so we can keep you updated.</div>
+              <div class="p2">Don’t see the confirmation email yet? It might be in your spam folder. If so, make sure to mark it as “not spam”.</div>
             </div>
-          </div>
-          <div class="page">
-            <div class="h1">Almost there…</div>
-            <div class="p1">You should get a confirmation email for each of your selected interests. Open it up and click ‘Confirm Subscription’ so we can keep you updated.</div>
-            <div class="p2">Don’t see the confirmation email yet? It might be in your spam folder. If so, make sure to mark it as “not spam”.</div>
-          </div> -->
+          </transition>
         </div>
       </div>
     </div>
@@ -40,6 +56,9 @@
 </template>
 
 <style scoped>
+a {
+  color: #66A1FF;
+}
 .container {
   background: url("./BackgroundStars.svg") repeat, linear-gradient(145.11deg, #202854 9.49%, #171B39 91.06%);
   font-family: var(--ds-font-family, sans-serif);
@@ -70,7 +89,18 @@
 .text {
   grid-column-start: 2;
   max-width: 36rem;
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+.page {
+  box-sizing: border-box;
   padding-right: 1rem;
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  align-items: center;
 }
 .h1 {
   font-size: 2rem;
@@ -79,11 +109,23 @@
   color: white;
   margin-bottom: 1.5rem;
 }
+.h2 {
+  font-size: 2rem;
+  color: white;
+  font-weight: 400;
+  line-height: 1.25;
+  margin-bottom: 2.5rem;
+}
 .p1 {
   color: rgba(255, 255, 255, 0.8);
   font-size: 1.25rem;
   line-height: 1.4;
   font-weight: 400;
+}
+.p2 {
+  font-size: .875rem;
+  color: rgba(255, 255, 255, 0.5);
+  line-height: 1.125rem;
 }
 .email__form {
   display: grid;
@@ -94,6 +136,7 @@
   margin-bottom: 1.5rem;
 }
 .email__form__input__input {
+  outline: none;
   width: 100%;
   background: rgba(255,255,255,.1);
   border: none;
@@ -113,18 +156,66 @@
 .email__form__input__input::placeholder {
   color: rgba(255, 255, 255, 0.5);
 }
+.forwards-enter-active,
+.forwards-leave-active,
+.backwards-enter-active,
+.backwards-leave-active {
+  transition: all .5s;
+}
+.forwards-enter {
+  opacity: 0;
+  transform: translateY(20px);
+}
+.forwards-enter-to {
+  opacity: 1;
+  transform: translateY(0);
+}
+.forwards-leave {
+  opacity: 1;
+  transform: translateY(0);
+}
+.forwards-leave-to {
+  opacity: 0;
+  transform: translateY(-20px);
+}
 </style>
 
 <script>
 import GraphicsPlanes from "./GraphicsPlanes"
 import GraphicsMail from "./GraphicsMail"
-import Button from "../Button/Button"
+import DsButton from "./DsButton"
+import IconArrowRight from "../Icons/IconArrowRight"
+import IconChevronLeft from "../Icons/IconChevronLeft"
 
 export default {
+  props: {
+    banner: {
+      type: Boolean,
+      default: true
+    }
+  },
   components: {
     GraphicsPlanes,
     GraphicsMail,
-    "ds-button": Button
+    DsButton,
+    IconArrowRight,
+    IconChevronLeft
+  },
+  data: function() {
+    return {
+      step: 0,
+      transition: "forwards"
+    }
+  },
+  methods: {
+    actionSubmit() {
+      this.transition = "forwards"
+      this.step += 1
+    },
+    actionBack() {
+      this.transition = "backwards"
+      this.step -= 1
+    }
   }
 }
 </script>
