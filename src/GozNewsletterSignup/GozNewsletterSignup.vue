74b7a07e93e454231bd40aeefd9f78277b87eaf6
Cyrus Goh <hello@lovincyrus.com>
2020-04-16 15:37:31 -0700

    remove max-width of goz-newsletter-signup container

--- src/GozNewsletterSignup/GozNewsletterSignup.vue
+++ src/GozNewsletterSignup/GozNewsletterSignup.vue
@@ -80,7 +80,6 @@
   padding: 4rem 1rem;
   box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.05), 0px 2px 6px rgba(0, 0, 0, 0.05), 0px 1px 0px rgba(0, 0, 0, 0.05);
   border-radius: .75rem;
-  max-width: 75rem;
   margin-left: auto;
   margin-right: auto;
   overflow-x: hidden;
